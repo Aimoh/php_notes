203a8a4a4ec180f8f2cbfda778e09db20346f575
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-05-18 17:55:47 +0500

    first

---
+++ Http/controllers/notes/destroy.php
@@ -0,0 +1,22 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query('delete from notes where id = :id', [
+    'id' => $_GET['id']
+]);
+
+header('location: /notes');
+exit();
+
